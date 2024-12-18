5ee8dd8cbad3f9173256237ad58484054a2ac57f
Anthony Jarina <anthony.jarina@gmail.com>
2024-12-18 17:42:07 -0600

    basic game structure with a Game class

--- main.py
+++ main.py
@@ -4,3 +4,30 @@ from os import walk
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 TILE_SIZE = 64
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Survivor')
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def run(self):
+        while self.running:
+            # dt
+            dt = self.clock.tick() / 1000
+
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            # update
+            pygame.display.update()
+
+        pygame.quit()
+
+game = Game()
+game.run()
+
+            # draw
